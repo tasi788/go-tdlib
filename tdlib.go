5f795ef1a5d01fdbcf4c2f601335616111cfa223
c0re100 <corehusky@gmail.com>
2019-10-28 21:52:32 +0800

    fix content nil

--- tdlib.go
+++ tdlib.go
@@ -291,26 +291,28 @@ func (client *Client) SendAndCatch(jsonQuery interface{}) (UpdateMsg, error) {
 			var messageDummy Message
 			json.Unmarshal(response.Raw, &messageDummy)
 
-			if messageDummy.Content.GetMessageContentEnum() == "messageText" {
-				msgWaiter := make(chan UpdateMsg, 1)
-				client.msgWaiters.Store(messageDummy.Id, msgWaiter)
+			if messageDummy.Content != nil {
+				if messageDummy.Content.GetMessageContentEnum() == "messageText" {
+					msgWaiter := make(chan UpdateMsg, 1)
+					client.msgWaiters.Store(messageDummy.Id, msgWaiter)
 
-				select {
-				case updateResp := <-msgWaiter:
-					var successMsg UpdateMessageSendSucceeded
-					json.Unmarshal(updateResp.Raw, &successMsg)
+					select {
+					case updateResp := <-msgWaiter:
+						var successMsg UpdateMessageSendSucceeded
+						json.Unmarshal(updateResp.Raw, &successMsg)
 
-					response.Data = updateResp.Data["message"].(map[string]interface {})
-					if str, err := json.Marshal(updateResp.Data["message"].(map[string]interface {})); err == nil {
-						response.Raw = []byte(str)
-					} else {
-						response.Raw = bytes.Replace(response.Raw, []byte("{\"@type\":\"messageSendingStatePending\"}"), []byte("{\"@type\":\"updateMessageSendSucceeded\"}"), 1)
-						response.Raw = bytes.Replace(response.Raw, []byte(strconv.FormatInt(messageDummy.Id, 10)), []byte(strconv.FormatInt(successMsg.Message.Id, 10)), 1)
+						response.Data = updateResp.Data["message"].(map[string]interface {})
+						if str, err := json.Marshal(updateResp.Data["message"].(map[string]interface {})); err == nil {
+							response.Raw = []byte(str)
+						} else {
+							response.Raw = bytes.Replace(response.Raw, []byte("{\"@type\":\"messageSendingStatePending\"}"), []byte("{\"@type\":\"updateMessageSendSucceeded\"}"), 1)
+							response.Raw = bytes.Replace(response.Raw, []byte(strconv.FormatInt(messageDummy.Id, 10)), []byte(strconv.FormatInt(successMsg.Message.Id, 10)), 1)
+						}
+
+						return response, nil
+					case <-time.After(1 * time.Second):
+						client.msgWaiters.Delete(messageDummy.Id)
 					}
-
-					return response, nil
-				case <-time.After(1 * time.Second):
-					client.msgWaiters.Delete(messageDummy.Id)
 				}
 			}
 		}
